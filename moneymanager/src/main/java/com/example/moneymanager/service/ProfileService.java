6df6eecc772e02c218404e376c09f400f668d323
Rahul Prabhu <rahulprabhu198@gmail.com>
2025-12-08 13:55:22 -0800

    created the 'send email for verificaiton' API

--- moneymanager/src/main/java/com/example/moneymanager/service/ProfileService.java
+++ moneymanager/src/main/java/com/example/moneymanager/service/ProfileService.java
@@ -13,12 +13,18 @@ import java.util.UUID;
 public class ProfileService {
 
     private final ProfileRepository profileRepository;
+    private final EmailService emailService;
 
     public ProfileDTO registerProfile(ProfileDTO profileDTO){
 
         ProfileEntity newProfile = toEntity(profileDTO);
         newProfile.setActivationToken(UUID.randomUUID().toString());
         newProfile = profileRepository.save(newProfile);
+        //Send activation email
+        String activationLink = "http://localhost:8080/api/v1.0/activate?token=" + newProfile.getActivationToken();
+        String subject = "Activate your Money Manager Account";
+        String body = "Click on the link to activate your account: \n\n\n" + activationLink;
+        emailService.sendEmail(newProfile.getEmail(), subject, body);
         return toDTO(newProfile);
     }
 
